a371c7be9aad7e1ee52bc798e25eced190f47f67
Alexander1-hash <74934158+Alexander1-hash@users.noreply.github.com>
2025-08-01 14:31:59 +0100

    Create Index.js

---
+++ pages/Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Welcome to CashPilot</h1>
+      <p>Your gateway to online income and opportunity.</p>
+    </main>
+  );
+}
